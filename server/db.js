a2e625f5ab8baf6f6207aebde5064f4cb4e3fd2b
jr <lloyd.dale.jr@gmail.com>
2026-03-02 12:38:40 -0500

    initial commit

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:<REHGzBJ3X3EpUdUcJjZR2WTX>@172.21.100.104";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
